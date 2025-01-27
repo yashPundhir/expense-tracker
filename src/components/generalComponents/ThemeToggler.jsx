a20bf17f006100a159032e48b1ab50a13e33c47d
yashPundhir <yash.pundhir.prof@gmail.com>
2025-01-28 01:21:48 +0530

    do the following changes: add jotai to the project update favicon and title of HTML create theme atom create theme toggler

---
+++ src/components/generalComponents/ThemeToggler.jsx
@@ -0,0 +1,31 @@
+import { useAtom } from "jotai";
+
+import { themeAtom } from "@/atoms/theme/store";
+
+import "@/css/themeToggler.css";
+
+const ThemeToggler = () => {
+	const [theme, setTheme] = useAtom(themeAtom);
+
+	const handleThemeToggle = () => {
+		document.getElementById("htmlRoot").classList.remove("light", "dark");
+		if (theme === "dark") {
+			document.getElementById("htmlRoot").classList.add("light");
+			setTheme("light");
+		} else {
+			document.getElementById("htmlRoot").classList.add("dark");
+			setTheme("dark");
+		}
+	};
+
+	return (
+		<div>
+			<label className="switch rounded-full ring-1 ring-zinc-200 dark:ring-zinc-700 shadow-md shadow-zinc-300 dark:shadow-zinc-700">
+				<input type="checkbox" onChange={handleThemeToggle} />
+				<span className="slider"></span>
+			</label>
+		</div>
+	);
+};
+
+export default ThemeToggler;
