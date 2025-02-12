a32a2a3a832164a0c5778a76e247397fe0b8c5c7
yashPundhir <yash.pundhir.prof@gmail.com>
2025-02-12 23:58:12 +0530

    update form schema wrt required validations

--- src/models/expenseFormSchema.js
+++ src/models/expenseFormSchema.js
@@ -1,9 +1,25 @@
 import { z } from "zod";
 
-export const expenseFormSchema = z.object({
-	amount: z.number().positive().min(0.01, "Amount must be greater than 0"),
-	expenseDescription: z.string().min(1, "Description is required"),
-	expenseCategory: z.string().min(1, "Category is required"),
+export const transactionFormSchema = z.object({
+	amount: z
+		.number({
+			required_error: "Amount is required",
+			invalid_type_error: "Amount must be a number",
+		})
+		.positive()
+		.min(0.01, "Amount must be greater than 0"),
+	expenseDescription: z
+		.string({
+			required_error: "Description is required",
+			invalid_type_error: "Description must be a string",
+		})
+		.min(1, "Description is required"),
+	expenseCategory: z
+		.string({
+			required_error: "Category is required",
+			invalid_type_error: "Description must be a string",
+		})
+		.min(1, "Category is required"),
 	expenseType: z.enum(["debit", "credit"], {
 		errorMap: () => ({
 			message: "Expense type must be either debit or credit",
@@ -11,5 +27,8 @@ export const expenseFormSchema = z.object({
 	}),
 	recurring: z.boolean(),
 	// expenseDate: z.date(),
-	expenseDate: z.coerce.date(),
+	expenseDate: z.coerce.date({
+		required_error: "Pick a Date",
+		invalid_type_error: "That's not a date!",
+	}),
 });
